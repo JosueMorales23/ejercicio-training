d8bfceab7098ec0f8e92223b76de2d0fe6f6fc67
JosueMorales23 <josue.morales071023@gmail.com>
2023-07-13 14:04:23 -0600

    Modificacion del archivo pocaInformacion para tomar la palabra unknown como poca informacion

--- scripts/pocaInformacion.py
+++ scripts/pocaInformacion.py
@@ -3,6 +3,8 @@ import pandas as pd
 # Carga de datos
 vh = pd.read_csv('./recursos/vehicles.csv')
 
+vh.replace('unknown', float("NaN"), inplace=True)
+
 print("Borra filas con menos del 50 de datos")
 cantidad = len(vh.columns) * 0.5
 datosBorrar = vh[vh.isna().sum(axis=1) >= cantidad]
